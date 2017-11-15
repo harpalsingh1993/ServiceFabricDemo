f30a93679c0d7a1c126942c0adafe92961c29eec
Uma Shankar <umashankar.itn@gmail.com>
2017-11-15 12:34:11 +0530

    Added ActorService.

---
+++ ECommerce.API/Controllers/BasketController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ECommerce.API.Models;
+using Microsoft.ServiceFabric.Actors.Client;
+using Microsoft.ServiceFabric.Actors;
+using UserActorService.Interfaces;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class BasketController : Controller
+    {
+        [HttpGet("{userId}")]
+        public async Task<ApiBasket> Get(string userId)
+        {
+            try
+            {
+                IUserActorService userActor = GetActor(userId);
+                Dictionary<Guid, int> products = await userActor.GetBasket();
+                return new ApiBasket()
+                {
+                    UserId = userId,
+                    Items = products.Select(
+                        p => new ApiBasketItem { ProductId = p.Key.ToString(), Quantity = p.Value }).ToArray()
+                };
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+
+        }
+
+        [HttpPost("{userId}")]
+        public async Task Add(string userId,[FromBody] ApiBasketAddRequest request)
+        {
+            IUserActorService userActor = GetActor(userId);
+            await userActor.AddToBasket(new Guid(request.ProductId),request.Qunatity);
+
+        }
+
+        [HttpDelete("{userId}")]
+        public async Task Delete(string userId)
+        {
+            IUserActorService userActor = GetActor(userId);
+            await userActor.ClearBasket();
+        }
+
+        private IUserActorService GetActor(string userId)
+        {
+            return ActorProxy.Create<IUserActorService>(
+                new ActorId(userId),
+                new Uri("fabric:/ECommerce/UserActorServiceActorService"));
+
+        }
+    }
+}
---
+++ ECommerce.API/Models/ApiBasketAddRequest.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Models
+{
+    public class ApiBasketAddRequest
+    {
+        [JsonProperty("productId")]
+        public string ProductId { get; set; }
+
+        [JsonProperty("qunatity")]
+        public int Qunatity { get; set; }
+
+    }
+}
